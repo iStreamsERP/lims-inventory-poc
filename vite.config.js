eb7fe621d765a2bca08504ca577a8fc34b50e699
Ajeeth <ajeetheswari14@gmail.com>
2025-03-25 17:46:19 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      "/public": {
+        target: "https://cloud.istreams-erp.com:4439/iStreamsSmartPublic.asmx",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/public/, ""),
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
