0b8c04e319eb90648397c69c90d3c2e481987b91
Ajeeth <ajeetheswari14@gmail.com>
2025-04-11 17:01:34 +0530

    srict mode removed

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import { AuthProvider } from "@/contexts/AuthContext.jsx";
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <AuthProvider>
-    <StrictMode>
-      <App />
-    </StrictMode>
+    <App />
   </AuthProvider>
 )
